4e02025eea8110b9193d04c9fbf55719e2e30b17
Talab Reach <reachtalab@gmail.com>
2026-07-12 16:10:31 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,12 @@
 <?php
-use App\Http\Controllers\StudentController;
 
-use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/users', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::get('/students', [StudentController::class, 'index']);       // Get all students
-Route::post('/students', [StudentController::class, 'store']);       // Create student
-Route::get('/students/{student}', [StudentController::class, 'show']); // Get one student
-Route::put('/students/{student}', [StudentController::class, 'update']); // Update student
-Route::patch('/students/{student}', [StudentController::class, 'update']); // Partial update
-Route::delete('/students/{student}', [StudentController::class, 'destroy']); // Delete student
+Route::get('/students', [StudentController::class, 'index']);
+Route::post('/students', [StudentController::class, 'store']);
+Route::get('/students/{student}', [StudentController::class, 'show']);
+Route::put('/students/{student}', [StudentController::class, 'update']);
+Route::patch('/students/{student}', [StudentController::class, 'update']);
+Route::delete('/students/{student}', [StudentController::class, 'destroy']);
 
